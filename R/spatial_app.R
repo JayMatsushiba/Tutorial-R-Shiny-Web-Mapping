e53312b8a47e1e6686f70bf8846a076a579a10f9
Jay Matsushiba <jay.matsushiba@gmail.com>
2024-09-26 23:07:57 -0700

    part 4 spatial shiny app

--- R/spatial_app.R
+++ R/spatial_app.R
@@ -1,17 +1,51 @@
 library(shiny)
+library(bslib)
 library(sf)
 library(leaflet)
 
 black_bear_obs <- st_read("../data/black_bear_observations_bc/black_bear_observations_bc.shp")
 
-ui <- fluidPage(
+# Define UI ----
+ui <- page_sidebar(
+  title = "Black Bear Observations Web Map",
+  sidebar = sidebar("Filter by Date",
+                    dateRangeInput(inputId = "date_range",
+                                   label = "Date Range"),
+                    actionButton(inputId = "filter_action",
+                                 label = "Filter"),
+                    # Adding some simple text outputs
+                    textOutput(outputId = "text_start_date"),
+                    textOutput(outputId = "text_end_date")
+  ),
   leafletOutput(outputId = "mymap")
 )
-server <- function(input, output, session) {
+
+# Define server logic ----
+server <- function(input, output) {
+  start_date <- eventReactive(input$filter_action, {
+    input$date_range[1]
+  })
+  end_date <- eventReactive(input$filter_action, {
+    input$date_range[2]
+  })
+  output$text_start_date <- renderText({
+    paste0("Start date: ", start_date())
+  })
+  output$text_end_date <- renderText({
+    paste0("End date: ",  end_date())
+  })
+
+  selected_black_bear_obs <- reactive({
+    black_bear_obs[black_bear_obs$observed_1 >= start_date() & black_bear_obs$observed_1 <= end_date(),]
+  })
+
   output$mymap <- renderLeaflet({
     leaflet() %>%
-      # This is how we add a basemap on the leaflet
-      addProviderTiles(provider = providers$CartoDB.Positron)
+      addProviderTiles(provider = providers$CartoDB.Positron) %>%
+      addCircles(data = selected_black_bear_obs(),
+                 popup = ~observed_1)
   })
 }
-shinyApp(ui, server)
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
