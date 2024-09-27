8bebb695671ed278e236629a94bf9d04110b17fe
Jay Matsushiba <jay.matsushiba@gmail.com>
2024-09-26 21:51:45 -0700

    base map section

--- R/spatial_app.R
+++ R/spatial_app.R
@@ -1,18 +1,17 @@
 library(shiny)
+library(sf)
 library(leaflet)
 
 black_bear_obs <- st_read("../data/black_bear_observations_bc/black_bear_observations_bc.shp")
 
 ui <- fluidPage(
-  # We can use leafletOutput like the other outputs
-  # Just need to pass a value to outputId
   leafletOutput(outputId = "mymap")
 )
 server <- function(input, output, session) {
-  # Creating the output to mymap, which is our leafletOutput
   output$mymap <- renderLeaflet({
-    # We create the leaflet object that will be displayed in the app
-    leaflet()
+    leaflet() %>%
+      # This is how we add a basemap on the leaflet
+      addProviderTiles(provider = providers$CartoDB.Positron)
   })
 }
 shinyApp(ui, server)
